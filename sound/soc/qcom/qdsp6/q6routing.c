297747c1713b42a921216dc9b12720190c49676f
Srinivas Kandagatla <srinivas.kandagatla@oss.qualcomm.com>
2025-09-12 18:19:05 +0100

    ASoC: qcom: q6dsp: add 32 bit support for i2s backend dais
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@oss.qualcomm.com>

--- sound/soc/qcom/qdsp6/q6routing.c
+++ sound/soc/qcom/qdsp6/q6routing.c
@@ -1083,6 +1083,9 @@ static int routing_hw_params(struct snd_soc_component *component,
 	case SNDRV_PCM_FORMAT_S24_LE:
 			session->bits_per_sample = 24;
 		break;
+	case SNDRV_PCM_FORMAT_S32_LE:
+			session->bits_per_sample = 32;
+		break;
 	default:
 		break;
 	}
