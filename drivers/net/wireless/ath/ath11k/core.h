a00b912e3d4b6d58aa4732eea829fb321c0d23c6
Miaoqing Pan <miaoqing.pan@oss.qualcomm.com>
2025-11-14 12:10:22 +0800

    FROMLIST: wifi: ath11k: add usecase firmware handling based on device compatible
    For M.2 WLAN chips, there is no suitable DTS node to specify the
    firmware-name property. In addition, assigning firmware for the
    M.2 PCIe interface causes chips that do not use usecase specific
    firmware to fail. Therefore, abandoning the approach of specifying
    firmware in DTS. As an alternative, propose a static lookup table
    mapping device compatible to firmware names. Currently, only WCN6855
    HW2.1 requires this.
    
    For details on usecase specific firmware, see:
    https://lore.kernel.org/all/20250522013444.1301330-3-miaoqing.pan@oss.qualcomm.com/.
    
    Tested-on: WCN6855 hw2.1 PCI WLAN.HSP.1.1-04685-QCAHSPSWPL_V1_V2_SILICONZ_IOE-1
    
    Fixes: edbbc647c4f3 ("wifi: ath11k: support usercase-specific firmware overrides")
    Signed-off-by: Miaoqing Pan <miaoqing.pan@oss.qualcomm.com>
    Link: https://lore.kernel.org/linux-wireless/20251204071100.970518-2-miaoqing.pan@oss.qualcomm.com/

--- drivers/net/wireless/ath/ath11k/core.h
+++ drivers/net/wireless/ath/ath11k/core.h
@@ -1292,6 +1292,7 @@ bool ath11k_core_coldboot_cal_support(struct ath11k_base *ab);
 
 const struct firmware *ath11k_core_firmware_request(struct ath11k_base *ab,
 						    const char *filename);
+const char *ath11k_core_get_usecase_firmware(struct ath11k_base *ab);
 
 static inline const char *ath11k_scan_state_str(enum ath11k_scan_state state)
 {
@@ -1342,9 +1343,7 @@ static inline void ath11k_core_create_firmware_path(struct ath11k_base *ab,
 						    const char *filename,
 						    void *buf, size_t buf_len)
 {
-	const char *fw_name = NULL;
-
-	of_property_read_string(ab->dev->of_node, "firmware-name", &fw_name);
+	const char *fw_name = ath11k_core_get_usecase_firmware(ab);
 
 	if (fw_name && strncmp(filename, "board", 5))
 		snprintf(buf, buf_len, "%s/%s/%s/%s", ATH11K_FW_DIR,
