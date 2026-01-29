8c25a5d458d2476d203bd851ea4baba4db96d244
hangtian <hangtian@oss.qualcomm.com>
2026-01-29 16:42:43 +0800

    wifi: ath12k: revert old 21 commits and prepare for sync to ath-next
    717323aec869 FROMLIST: wifi: ath12k: Fix scan state stuck in ABORTING
    45298a021956 FROMLIST: wifi: ath12k: Fix wrong P2P device link id issue
    6b4cd38084fc FROMLIST: wifi: ath11k: Fix failure to connect to a 6 GHz AP
    0362e9ee4555 BACKPORT: FROMLIST: wifi: ath12k: enable QCC2072 support
    b6173fded283 BACKPORT: FROMLIST: wifi: ath12k: fix PCIE_LOCAL_REG_QRTR_NODE_ID definition for QCC2072
    bd29c5d5887e BACKPORT: FROMLIST: wifi: ath12k: send peer meta data version to firmware
    a36f6d66fabc BACKPORT: FROMLIST: wifi: ath12k: limit number of channels per WMI command
    ce9011da1b27 BACKPORT: FROMLIST: wifi: ath12k: add hardware ops support for QCC2072
    f9027280969d BACKPORT: FROMLIST: wifi: ath12k: add HAL descriptor and ops for QCC2072
    213b68004a18 BACKPORT: FROMLIST: wifi: ath12k: support downloading auxiliary ucode image for QCC2072
    8f837c9c1e23 BACKPORT: FROMLIST: wifi: ath12k: support LPASS_SHARED target memory type
    88be57fadcfa BACKPORT: FROMLIST: wifi: ath12k: advertise standard ELF image format for QCC2072
    4295f50aff40 BACKPORT: FROMLIST: wifi: ath12k: add hardware parameters for QCC2072
    adda2a1c1526 BACKPORT: FROMLIST: wifi: ath12k: add hardware registers for QCC2072
    549b258303f9 BACKPORT: FROMLIST: wifi: ath12k: fix mac phy capability parsing
    ef75d0fa17e0 BACKPORT: FROMLIST: wifi: ath12k: refactor 320 MHz bandwidth support parsing
    74bb59f99219 BACKPORT: FROMLIST: wifi: ath12k: fix preferred hardware mode calculation
    3475d1baacdb BACKPORT: FROMLIST: wifi: ath12k: refactor REO status ring handling
    19baa021bd91 BACKPORT: FROMLIST: wifi: ath12k: refactor REO CMD ring handling
    995b87589b55 BACKPORT: FROMLIST: wifi: ath12k: refactor PCI window register access
    63d6e1c2c487 FROMLIST: wifi: ath11k: add usecase firmware handling based on device compatible

--- drivers/net/wireless/ath/ath11k/core.h
+++ drivers/net/wireless/ath/ath11k/core.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: BSD-3-Clause-Clear */
 /*
  * Copyright (c) 2018-2019 The Linux Foundation. All rights reserved.
- * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * Copyright (c) 2021-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef ATH11K_CORE_H
@@ -1275,7 +1275,6 @@ bool ath11k_core_coldboot_cal_support(struct ath11k_base *ab);
 
 const struct firmware *ath11k_core_firmware_request(struct ath11k_base *ab,
 						    const char *filename);
-const char *ath11k_core_get_usecase_firmware(struct ath11k_base *ab);
 
 static inline const char *ath11k_scan_state_str(enum ath11k_scan_state state)
 {
@@ -1326,7 +1325,9 @@ static inline void ath11k_core_create_firmware_path(struct ath11k_base *ab,
 						    const char *filename,
 						    void *buf, size_t buf_len)
 {
-	const char *fw_name = ath11k_core_get_usecase_firmware(ab);
+	const char *fw_name = NULL;
+
+	of_property_read_string(ab->dev->of_node, "firmware-name", &fw_name);
 
 	if (fw_name && strncmp(filename, "board", 5))
 		snprintf(buf, buf_len, "%s/%s/%s/%s", ATH11K_FW_DIR,
