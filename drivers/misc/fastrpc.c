61ae30cdece0c8b2d1339c99e96bda9e22c261a9
Ekansh Gupta <ekansh.gupta@oss.qualcomm.com>
2026-05-26 19:11:24 +0800

    FROMLIST: misc: fastrpc: Allow fastrpc_buf_free() to accept NULL
    Make fastrpc_buf_free() a no-op when passed a NULL pointer, allowing
    callers to avoid open-coded NULL checks.
    
    Link: https://lore.kernel.org/all/20260526111124.515-6-jianping.li@oss.qualcomm.com/
    Signed-off-by: Ekansh Gupta <ekansh.gupta@oss.qualcomm.com>
    Signed-off-by: Jianping Li <jianping.li@oss.qualcomm.com>
    Signed-off-by: Vinayak Katoch <vkatoch@qti.qualcomm.com>

--- drivers/misc/fastrpc.c
+++ drivers/misc/fastrpc.c
@@ -585,8 +585,7 @@ static void fastrpc_context_free(struct kref *ref)
 	for (i = 0; i < ctx->nbufs; i++)
 		fastrpc_map_put(ctx->maps[i]);
 
-	if (ctx->buf)
-		fastrpc_buf_free(ctx->buf);
+	fastrpc_buf_free(ctx->buf);
 
 	spin_lock_irqsave(&cctx->lock, flags);
 	idr_remove(&cctx->ctx_idr, FIELD_GET(FASTRPC_CTXID_MASK, ctx->ctxid));
@@ -1728,8 +1727,7 @@ static int fastrpc_device_release(struct inode *inode, struct file *file)
 	list_del(&fl->user);
 	spin_unlock_irqrestore(&cctx->lock, flags);
 
-	if (fl->init_mem)
-		fastrpc_buf_free(fl->init_mem);
+	fastrpc_buf_free(fl->init_mem);
 
 	list_for_each_entry_safe(ctx, n, &fl->pending, node) {
 		list_del(&ctx->node);
