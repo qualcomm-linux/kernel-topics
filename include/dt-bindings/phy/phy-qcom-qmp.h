3bdd6c4c66e7d693fa9e1f78a541a6d6c3c3beee
Krishna Chaitanya Chundru <krishna.chundru@oss.qualcomm.com>
2026-08-10 20:59:42 +0530

    dt-bindings: phy: qcom,qmp-pcie: Add Nord PHY selector and link-mode macros
    Add the generic QMP_PHY_SELECTOR_* constants used to select a logical
    PHY instance out of a multi-PHY provider, and the Nord-specific
    QMP_PCIE_NORD_MODE_* constants describing the TCSR_PCIE_LINK_CONFIG_MODE
    encoding read by the Nord QMP PCIe multi-PHY driver.
    
    Signed-off-by: Krishna Chaitanya Chundru <krishna.chundru@oss.qualcomm.com>

--- include/dt-bindings/phy/phy-qcom-qmp.h
+++ include/dt-bindings/phy/phy-qcom-qmp.h
@@ -21,4 +21,19 @@
 #define QMP_PCIE_PIPE_CLK		0
 #define QMP_PCIE_PHY_AUX_CLK		1
 
+/* Generic QMP logical PHY selectors */
+#define QMP_PHY_SELECTOR_0		0
+#define QMP_PHY_SELECTOR_1		1
+#define QMP_PHY_SELECTOR_2		2
+#define QMP_PHY_SELECTOR_3		3
+
+/*
+ * Nord QMP PCIe link-mode values (TCSR_PCIE_LINK_CONFIG_MODE)
+ * These match the hardware register encoding programmed by the bootloader.
+ */
+#define QMP_PCIE_NORD_MODE_X16		0  /* all 16 lanes -> single x16 port        */
+#define QMP_PCIE_NORD_MODE_X8_X8	1  /* x8 (port A) + x8 (ports B+C+D ganged)  */
+#define QMP_PCIE_NORD_MODE_X8_X4_X4	2  /* x8 (A) + x4 (B) + x4 (C+D ganged)   */
+#define QMP_PCIE_NORD_MODE_X8_X4_X2_X2	3  /* x8 (A) + x4 (B) + x2 (C) + x2 (D)  */
+
 #endif /* _DT_BINDINGS_PHY_QMP */
