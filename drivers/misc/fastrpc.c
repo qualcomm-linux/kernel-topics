78164073db67ede4f3bc47ef00c43a95d3537fdb
Vinayak Katoch <vinayak.katoch@oss.qualcomm.com>
2026-08-26 18:55:50 +0530

    FROMLIST: misc: fastrpc: move ADSP duplicate session creation to the driver
    For ADSP, only a limited number of FastRPC context banks (CBs) are
    available. Each CB supports a single session, which means only a few
    processes can run on ADSP simultaneously. If all sessions are consumed
    by fastrpc daemons, no session remains available when a user application
    starts, causing the application to fail.
    
    Previously, duplicate sessions were created per CB via the qcom,nsessions
    DT property during fastrpc_cb_init(). Now that CB initialisation is
    synchronous, create FASTRPC_DUP_SESSIONS duplicates of the last ADSP
    session in fastrpc_rpmsg_probe() after all CBs are initialised. Remove
    the per-CB duplication logic and the qcom,nsessions read from
    fastrpc_cb_init().
    
    Link: https://lore.kernel.org/all/20260826-dup-sessions-v4-2-35555d2bfed4@oss.qualcomm.com/
    Signed-off-by: Vinayak Katoch <vinayak.katoch@oss.qualcomm.com>

--- drivers/misc/fastrpc.c
+++ drivers/misc/fastrpc.c
@@ -34,6 +34,7 @@
 #define CDSP_DOMAIN_ID (3)
 #define GDSP_DOMAIN_ID (4)
 #define FASTRPC_MAX_SESSIONS	14
+#define FASTRPC_DUP_SESSIONS	4
 #define FASTRPC_MAX_VMIDS	16
 #define FASTRPC_ALIGN		128
 #define FASTRPC_MAX_FDLIST	16
@@ -2331,16 +2332,17 @@ static int fastrpc_cb_init(struct platform_device *pdev)
 	struct fastrpc_channel_ctx *cctx;
 	struct fastrpc_session_ctx *sess;
 	struct device *dev = &pdev->dev;
-	int i, sessions = 0;
 	unsigned long flags;
 	int rc;
 	u32 dma_bits;
+	u32 sid = 0;
 
 	cctx = dev_get_drvdata(dev->parent);
 	if (!cctx)
 		return -EINVAL;
 
-	of_property_read_u32(dev->of_node, "qcom,nsessions", &sessions);
+	if (of_property_read_u32(dev->of_node, "reg", &sid))
+		dev_info(dev, "FastRPC Session ID not specified in DT\n");
 
 	spin_lock_irqsave(&cctx->lock, flags);
 	if (cctx->sesscount >= FASTRPC_MAX_SESSIONS) {
@@ -2353,24 +2355,12 @@ static int fastrpc_cb_init(struct platform_device *pdev)
 	sess->used = false;
 	sess->valid = true;
 	sess->dev = dev;
+	sess->sid = sid;
 	dev_set_drvdata(dev, sess);
 
 	if (cctx->domain_id == CDSP_DOMAIN_ID)
 		dma_bits = cctx->soc_data->dma_addr_bits_cdsp;
 
-	if (of_property_read_u32(dev->of_node, "reg", &sess->sid))
-		dev_info(dev, "FastRPC Session ID not specified in DT\n");
-
-	if (sessions > 0) {
-		struct fastrpc_session_ctx *dup_sess;
-
-		for (i = 1; i < sessions; i++) {
-			if (cctx->sesscount >= FASTRPC_MAX_SESSIONS)
-				break;
-			dup_sess = &cctx->session[cctx->sesscount++];
-			memcpy(dup_sess, sess, sizeof(*dup_sess));
-		}
-	}
 	spin_unlock_irqrestore(&cctx->lock, flags);
 	rc = dma_set_mask(dev, DMA_BIT_MASK(dma_bits));
 	if (rc) {
@@ -2676,6 +2666,22 @@ static int fastrpc_rpmsg_probe(struct rpmsg_device *rpdev)
 		err = fastrpc_cb_init(pdev);
 		if (err)
 			goto err_depopulate;
+	}
+
+	if (data->domain_id == ADSP_DOMAIN_ID && data->sesscount > 0) {
+		struct fastrpc_session_ctx *last_sess;
+		struct fastrpc_session_ctx *dup_sess;
+		unsigned long flags;
+
+		spin_lock_irqsave(&data->lock, flags);
+		last_sess = &data->session[data->sesscount - 1];
+		for (i = 0; i < FASTRPC_DUP_SESSIONS; i++) {
+			if (data->sesscount >= FASTRPC_MAX_SESSIONS)
+				break;
+			dup_sess = &data->session[data->sesscount++];
+			memcpy(dup_sess, last_sess, sizeof(*dup_sess));
+		}
+		spin_unlock_irqrestore(&data->lock, flags);
 	}
 
 	return 0;
